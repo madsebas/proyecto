4f7a2780594d41384c79b46ee0a00e3890fd2a9d
Manuel <madare2009@gmail.com>
2019-04-07 00:50:36 -0500

    dormir

--- ejemplo/src/ejemplo/Ejemplo.java
+++ ejemplo/src/ejemplo/Ejemplo.java
@@ -3,7 +3,8 @@ package ejemplo;
 public class Ejemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hola jovenes");
+        System.out.println("Hola jovenessaluy");
+        System.out.println("Ya tengo que dormir");
     }
 
 }
